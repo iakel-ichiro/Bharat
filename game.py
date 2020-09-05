9a0ed07e7da076cbd622f02722659e3f6cfb0921
Kai <ichigo.ryukiv@gmail.com>
2020-09-05 14:16:53 +0100

    created the title user input function

--- game.py
+++ game.py
@@ -9,15 +9,31 @@ import random # contains rnd functions
 
 screen_width = 100
 
-
 ### Title Screen ###
 def title_screen():
     os.system('cls') # clears the commad prompt
     print('####################################')
-    print('           Welcome to Uya           ')
+    print('           Welcome to Ullysis           ')
     print('####################################')
     print('              - Start -             ')
     print('              - Help -              ')
     print('              -Exit -               ')
+    title_choice()
+### Title Screen Choice ###
+def title_choice():
+    choice = input('> ')
+    while choice.lower() not in ['start', 'help', 'exit']:
+        print('I do not understand, please choose again.')
+        choice = input('> ')
+    if choice.lower() == 'start':
+        start_game()
+    elif choice.lower() == 'help':
+        help_menu()
+    elif choice.lower() == 'exit':
+        exit_game()
+
+
+
+
 
 title_screen()
