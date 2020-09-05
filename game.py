45187279ce35722a29c15a5f62d089c92ac0f69a
Kai <ichigo.ryukiv@gmail.com>
2020-09-05 14:09:26 +0100

    Created the title screen function, which prints options and clears the screen

--- game.py
+++ game.py
@@ -2,8 +2,22 @@
 ### Imports ###
 import cmd # allows command prompt fnction
 import textwrap # wraps text to command prompt screen
+import os
 import sys # allows system functions
 import time # contains counter functions
 import random # contains rnd functions
 
 screen_width = 100
+
+
+### Title Screen ###
+def title_screen():
+    os.system('cls') # clears the commad prompt
+    print('####################################')
+    print('           Welcome to Uya           ')
+    print('####################################')
+    print('              - Start -             ')
+    print('              - Help -              ')
+    print('              -Exit -               ')
+
+title_screen()
