0786beaf89aa56643b10480b5c5f7ab749f79b4e
Kai <ichigo.ryukiv@gmail.com>
2020-09-05 14:00:45 +0100

    Added imports and screenwidth

--- game.py
+++ game.py
@@ -1,2 +1,9 @@
 # Text RPG Attempt 2
-print('hello world')
+### Imports ###
+import cmd # allows command prompt fnction
+import textwrap # wraps text to command prompt screen
+import sys # allows system functions
+import time # contains counter functions
+import random # contains rnd functions
+
+screen_width = 100
